3f7f1d65b63215c1e2f78c9a21681f242d610fc3
Steph <styu314@gmail.com>
2012-01-17 21:15:07 -0600

    added preliminary settings and user preferences, will redirect to main page on start up if kerberos id already set

--- src/edu/mit/printAtMIT/main/SettingsActivity.java
+++ src/edu/mit/printAtMIT/main/SettingsActivity.java
@@ -1,8 +1,12 @@
 package edu.mit.printAtMIT.main;
 
 import edu.mit.printAtMIT.R;
+import edu.mit.printAtMIT.PrintAtMITActivity;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Users can reset their kerberos ID here
@@ -15,5 +19,9 @@ public class SettingsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        
+        EditText username = (EditText) findViewById(R.id.entry);
+        SharedPreferences settings = getSharedPreferences(PrintAtMITActivity.PREFS_NAME, MODE_PRIVATE);
+        username.setText(settings.getString(PrintAtMITActivity.USERNAME, ""));
     }
 }
