67dc91876626a1c64df348cc93e748bf3e76e68f
Sarah Edris <sredris@gmail.com>
2012-01-18 00:00:18 -0500

    added list of downloads

--- src/edu/mit/printAtMIT/print/PrintOptionsActivity.java
+++ src/edu/mit/printAtMIT/print/PrintOptionsActivity.java
@@ -1,8 +1,9 @@
 package edu.mit.printAtMIT.print;
 
-import edu.mit.printAtMIT.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import edu.mit.printAtMIT.R;
 
 /**
  * User selects print options:
@@ -20,5 +21,14 @@ public class PrintOptionsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.print_options);
+        
+        // Show file chosen -- need to add options, then print
+        Bundle extras = getIntent().getExtras(); 
+        String fileLoc = extras.getString("fileLoc");
+        String fileName = extras.getString("fileName");
+        
+        Toast.makeText(getApplicationContext(), fileLoc, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), fileName, Toast.LENGTH_SHORT).show();
+
     }
 }
