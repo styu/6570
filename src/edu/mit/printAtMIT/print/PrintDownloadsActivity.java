7bb3a3a22262ca86dbdbadefd876ef44ac80d44e
Steph <styu314@gmail.com>
2012-01-19 21:43:16 -0600

    added alert dialog when selecting file to print

--- src/edu/mit/printAtMIT/print/PrintDownloadsActivity.java
+++ src/edu/mit/printAtMIT/print/PrintDownloadsActivity.java
@@ -2,7 +2,9 @@ package edu.mit.printAtMIT.print;
 
 import java.io.File;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -54,19 +56,36 @@ public class PrintDownloadsActivity extends FileViewActivity {
               // When clicked, show a toast with the TextView text
               //Toast.makeText(getApplicationContext(), files.listFiles()[position].toString(),
               //    Toast.LENGTH_SHORT).show();
-        	  
-        	  String fileName = files.list()[position];
-        	  
+        	  final String fileName = files.list()[position];
+        	  final int pos = position;
+        	  final View v = view;
         	  // Must be correct format
         	  if (fileName.endsWith(".pdf") || fileName.endsWith(".ps") || fileName.endsWith(".txt")) {
-        		  Intent intent = new Intent(view.getContext(), PrintOptionsActivity.class);
-        		  intent.putExtra("fileLoc", files.listFiles()[position].toString());
-        		  intent.putExtra("fileName", fileName);
-        		  startActivity(intent);
+        		  
+        		  AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+            	  builder.setMessage("Do you want to print this file?")
+            	         .setCancelable(false)
+            	         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            	             public void onClick(DialogInterface dialog, int id) {
+            	            	 Intent intent = new Intent(v.getContext(), PrintOptionsActivity.class);
+            	        		  intent.putExtra("fileLoc", files.listFiles()[pos].toString());
+            	        		  intent.putExtra("fileName", fileName);
+            	        		  startActivity(intent);
+            	             }
+            	         })
+            	         .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            	             public void onClick(DialogInterface dialog, int id) {
+            	                  dialog.cancel();
+            	             }
+            	         });
+            	  AlertDialog alert = builder.create();
+            	  alert.show();
+        		 
         	  }
         	  else {
                   Toast.makeText(getApplicationContext(), "Invalid file type", Toast.LENGTH_SHORT).show();
         	  }
+        
             }
           });
     }
