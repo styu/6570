5eaedcfe6ae5997de1db2579482383f068ef6164
Qian Long <hungryha@gmail.com>
2012-01-27 23:42:53 -0500

    added refresh and menu items to MapActivity

--- src/edu/mit/printAtMIT/view/PrinterMapActivity.java
+++ src/edu/mit/printAtMIT/view/PrinterMapActivity.java
@@ -13,10 +13,24 @@ import com.google.android.maps.OverlayItem;
 import com.google.android.maps.ItemizedOverlay;
 
 import edu.mit.printAtMIT.R;
+import edu.mit.printAtMIT.main.MainMenuActivity;
+import edu.mit.printAtMIT.main.SettingsActivity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -38,41 +52,142 @@ public class PrinterMapActivity extends MapActivity {
     Drawable drawable;
     PrinterItemizedOverlay itemizedOverlay;
     FixedMyLocationOverlay myLocationOverlay;
-    private List<ParseObject> mPrinters;
     public boolean tapped_overlay = false;
+    private boolean allView;
+    private int centerLat;
+    private int centerLong;
+    private Bundle extras;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mPrinters = new ArrayList<ParseObject>();
         Parse.initialize(this, "KIb9mNtPKDtkDk7FJ9W6b7MiAr925a10vNuCPRer",
                 "dSFuQYQXSvslh9UdznzzS9Vb0kDgcKnfzgglLUHT");
         setContentView(R.layout.map);
-        Bundle extras = getIntent().getExtras();
+        extras = getIntent().getExtras();
+        allView = extras.getBoolean("allPrinterView", false);
 
-        Log.i(TAG, "creating ParseQuery PrintersData");
-        ParseQuery query = new ParseQuery("PrintersData");
-        Log.i(TAG, "created ParseQuery PrintersData");
-        boolean allView = extras.getBoolean("allPrinterView", false);
+        centerLat = MIT_CENTER_LAT;
+        centerLong = MIT_CENTER_LONG;
 
-        int centerLat = MIT_CENTER_LAT;
-        int centerLong = MIT_CENTER_LONG;
-        if (allView) {
-            setPrinterList(query);
-        } else {
-            String id = extras.getString("id");
-            ParseObject printer;
-            try {
-                printer = query.get(id);
-                centerLat = Integer.parseInt(printer.getString("latitude"));
-                centerLong = Integer.parseInt(printer.getString("longitude"));
-                mPrinters.add(printer);
-            } catch (ParseException e) {
-                Log.e("PrinterMapActivity", "Parse query.get(id) FAIL");
-                e.printStackTrace();
+        if (isConnected()) {
+            ParseQuery query = new ParseQuery("PrintersData");
+            if (allView) {
+                refresh(getParseData(query, null));
+            } else {
+                refresh(getParseData(query, extras.getString("id")));
             }
         }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    "Error getting data, please try again later",
+                    Toast.LENGTH_SHORT).show();
+        }
 
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isConnected()) {
+            ParseQuery query = new ParseQuery("PrintersData");
+            if (allView) {
+                refresh(getParseData(query, null));
+            } else {
+                refresh(getParseData(query, extras.getString("id")));
+            }
+            if (myLocationOverlay != null) {
+                myLocationOverlay.enableMyLocation();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    "Error getting data, please try again later",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (myLocationOverlay != null) {
+            myLocationOverlay.disableMyLocation();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Intent intent;
+        switch (item.getItemId()) {
+        case R.id.refresh:
+            RefreshTask task = new RefreshTask();
+            if (allView) {
+                task.execute((String)null);
+            }
+            else {
+                task.execute(extras.getString("id"));
+            }
+            return true;
+        case R.id.home:
+            intent = new Intent(
+                    findViewById(android.R.id.content).getContext(),
+                    MainMenuActivity.class);
+            startActivity(intent);
+            return true;
+        case R.id.setting:
+            intent = new Intent(
+                    findViewById(android.R.id.content).getContext(),
+                    SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        case R.id.about:
+            Dialog dialog = new Dialog(this);
+
+            dialog.setContentView(R.layout.about_dialog);
+            dialog.setTitle("About");
+            dialog.show();
+            super.onOptionsItemSelected(item);
+            return true;
+        case R.id.list:
+            intent = new Intent(
+                    findViewById(android.R.id.content).getContext(),
+                    PrinterListActivity.class);
+            startActivity(intent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void refresh(List<ParseObject> objects) {
+        // if (allView) {
+        // setPrinterList(query, null);
+        // } else {
+        // String id = extras.getString("id");
+        // ParseObject printer;
+        // try {
+        // printer = query.get(id);
+        // centerLat = Integer.parseInt(printer.getString("latitude"));
+        // centerLong = Integer.parseInt(printer.getString("longitude"));
+        // mPrinters.add(printer);
+        // } catch (ParseException e) {
+        // Log.e("PrinterMapActivity", "Parse query.get(id) FAIL");
+        // e.printStackTrace();
+        // }
+        // }
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         mapOverlays = mapView.getOverlays();
@@ -100,7 +215,7 @@ public class PrinterMapActivity extends MapActivity {
         controller.setZoom(17);
         controller.animateTo(new GeoPoint(centerLat, centerLong));
 
-        Collections.sort(mPrinters, new Comparator<ParseObject>() {
+        Collections.sort(objects, new Comparator<ParseObject>() {
 
             @Override
             public int compare(ParseObject obj1, ParseObject obj2) {
@@ -110,7 +225,7 @@ public class PrinterMapActivity extends MapActivity {
 
         });
         // add printer overlayitems to map
-        for (ParseObject printer : mPrinters) {
+        for (ParseObject printer : objects) {
             Log.i(TAG, printer.getString("status"));
             GeoPoint point = new GeoPoint(Integer.parseInt(printer
                     .getString("latitude")), Integer.parseInt(printer
@@ -142,37 +257,31 @@ public class PrinterMapActivity extends MapActivity {
         mapOverlays.add(itemizedOverlay);
     }
 
-    @Override
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        myLocationOverlay.enableMyLocation();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        myLocationOverlay.disableMyLocation();
-    }
-
     /**
      * Makes request to Parse to retrieve list of all printers. Sets mPrinters
      * to that list.
+     * 
+     * @param query
+     *            : ParseQuery id: ParseId of printer, null if viewing all
+     *            printers
      */
-    private void setPrinterList(ParseQuery query) {
-
-        try {
-            List<ParseObject> objects = query.find();
-            for (ParseObject o : objects) {
-                mPrinters.add(o);
+    private List<ParseObject> getParseData(ParseQuery query, String id) {
+        List<ParseObject> objects = new ArrayList<ParseObject>();
+        if (id == null) {
+            try {
+                objects = query.find();
+            } catch (ParseException e1) {
+                Log.e(TAG, "query.find() FAILED");
             }
-        } catch (ParseException e1) {
-            Log.e(TAG, "query.find() FAILED");
+        } else {
+            try {
+                objects.add(query.get(id));
+            } catch (ParseException e) {
+                Log.e(TAG, "query.get(id) FAILED");
+            }
+
         }
+        return objects;
 
     }
 
@@ -184,5 +293,71 @@ public class PrinterMapActivity extends MapActivity {
         } else {
             return "Error";
         }
+    }
+
+    /**
+     * Checks to see if user is connected to wifi or 3g
+     * 
+     * @return
+     */
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) PrinterMapActivity.this
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+
+            networkInfo = connectivityManager
+                    .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+            if (!networkInfo.isAvailable()) {
+                networkInfo = connectivityManager
+                        .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            }
+        }
+        return networkInfo == null ? false : networkInfo.isConnected();
+    }
+
+    public class RefreshTask extends
+            AsyncTask<String, byte[], List<ParseObject>> {
+        private ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(TAG, "RefreshTask onPreExecute");
+            dialog = ProgressDialog.show(PrinterMapActivity.this, "",
+                    "Refreshing Data", true);
+        }
+
+        @Override
+        protected void onCancelled() {
+            Log.i(TAG, "RefreshTask Cancelled.");
+        }
+
+        @Override
+        protected List<ParseObject> doInBackground(String... params) {
+            if (isConnected()) {
+                ParseQuery query = new ParseQuery("PrintersData");
+                return getParseData(query, params[0]);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<ParseObject> result) {
+            if (result == null) {
+                result = new ArrayList<ParseObject>();
+            }
+            if (result.size() == 0) {
+                Toast.makeText(getApplicationContext(),
+                        "Error getting data, please try again later",
+                        Toast.LENGTH_SHORT).show();
+                Log.i(TAG,
+                        "RefreshTask onPostExecute: Completed with an Error.");
+            }
+            refresh(result);
+            dialog.dismiss();
+
+        }
+
     }
 }
